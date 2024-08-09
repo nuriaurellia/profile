6b46d4debbbf3263bb72caeb843fc622a34edb9c
nuriaurellia <146143830+nuriaurellia@users.noreply.github.com>
2024-08-09 23:00:22 +0700

    Add files via upload

---
+++ startbootstrap-personal-main/dist/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* My Portofolio - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 My Portofolio
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
